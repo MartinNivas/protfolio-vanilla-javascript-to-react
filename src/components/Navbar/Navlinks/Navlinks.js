e9106db360588b46f6f1415d69d7827254457ef6
Nivas <arokiarajnivas@gmail.com>
2021-10-09 13:15:28 +0800

    refactor(footer): added footer notes
    Footer copyright added new line with link

--- src/components/Navbar/Navlinks/Navlinks.js
+++ src/components/Navbar/Navlinks/Navlinks.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import Socialmedia from './Socialmedia/Socialmedia';
+import Socialmedia from '../../Socialmedia/Socialmedia';
 import { Link } from 'react-router-dom';
 
 const PAGES = [
